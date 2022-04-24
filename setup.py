52be0280dd268076237ddfe8c19767dd8433d7d7
Alex Q <alex.quan0807@gmail.com>
2022-04-24 19:43:41 -0400

    FIXED setup name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='compressortoolbox',
+    name='proxyscraperservicewrapper',
     version='0.0.1',
     author='Alex Q',
     author_email='alex.quan0807@gmail.com',
